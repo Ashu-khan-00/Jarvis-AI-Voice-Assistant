99ec69ba54b9d6dfeed4b6dcc47701511b5a5009
Mohd Ashraf Khan <mdashrafkhan9911@gmail.com>
2026-01-22 22:57:36 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "still": "https://youtu.be/rqSk9EaKuuM?si=gVZdMYvlo5clnAbh",
+    "reason": "https://youtu.be/-YlmnPh-6rE?si=M93MGxBXOCWiiLSe",
+    "song": "https://youtu.be/ogBQ3VymYbk?si=vNS_C05PiQAvY3q8",
+    "maharani" : "https://youtu.be/VhRJ-3T4Rcw?si=AdjUfs-RRKenjcu6"
+
+    }
